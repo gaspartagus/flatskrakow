9761cfead885c0cc468a48852c77ab1f112a5dfd
Gaspard Benoit <gaspartagus@users.noreply.github.com>
2017-06-09 16:32:13 +0200

    make it better

--- index.js
+++ index.js
@@ -239,6 +239,8 @@ var express = require('express');
 var app = express();
 
 
+app.set('port', (process.env.PORT || 5000))
+
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
